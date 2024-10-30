6a141cc8349b925b11621875dfffd6c3abd8dcd9
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-30 18:22:03 +0100

    Populated NavBar with NavLink elements and added basic page layout to App.tsx to display the NavBar. Pages now switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import NavBar from './Components/NavBar/NavBar.tsx';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Containers/Home/Home.tsx';
+import About from './Containers/About/About.tsx';
+import Contacts from './Containers/Contacts/Contacts.tsx';
+import Services from './Containers/Services/Services.tsx';
+import Membership from './Containers/Membership/Membership.tsx';
+import Login from './Containers/Login/Login.tsx';
 
 const App = () => {
 
   return (
     <>
+      <header>
+        <NavBar/>
+      </header>
+      <main className="container mt-4">
+        <div className="row">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/pages/services" element={<Services/>}/>
+            <Route path="/pages/membership" element={<Membership/>}/>
+            <Route path="/pages/login" element={<Login/>}/>
+            <Route path="/pages/about" element={<About/>}/>
+            <Route path="*" element={<h4>Not found</h4>}/>
+            <Route path="/pages/contacts" element={<Contacts/>}>
+            </Route>
+          </Routes>
+        </div>
+      </main>
 
     </>
   );
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,10 +1,40 @@
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
   return (
-    <div>
+    <>
+      <nav className="navbar bg-primary-subtle">
+        <div className="container">
+          <div className="row justify-content-between align-items-center">
+            <span className="navbar-brand mb-0 h1 text-primary-emphasis fs-1">Lantern</span>
+          </div>
+          <div className="ms-auto">
+            <nav className="navbar navbar-nav d-flex flex-row gap-3">
+              <li>
+                <NavLink to="/" className="text-primary-emphasis text-decoration-none">home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/pages/about" className="text-primary-emphasis text-decoration-none">about</NavLink>
+              </li>
+              <li>
+                <NavLink to="/pages/contacts" className="text-primary-emphasis text-decoration-none">contacts</NavLink>
+              </li>
+              <li>
+                <NavLink to="/pages/services" className="text-primary-emphasis text-decoration-none">services</NavLink>
+              </li>
+              <li>
+                <NavLink to="/pages/membership" className="text-primary-emphasis text-decoration-none">membership</NavLink>
+              </li>
+              <li>
+                <NavLink to="/pages/login" className="text-primary-emphasis text-decoration-none">login</NavLink>
+              </li>
+            </nav>
+          </div>
+        </div>
+      </nav>
 
-    </div>
+    </>
   );
 };
 
